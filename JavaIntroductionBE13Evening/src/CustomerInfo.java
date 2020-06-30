45e3c6ac63f97c13f69a98ede38f713e4bdce3e7
andron13 <andron13@gmail.com>
2020-06-30 19:35:14 +0200

    lesson-02 start

---
+++ JavaIntroductionBE13Evening/src/CustomerInfo.java
@@ -0,0 +1,17 @@
+public class CustomerInfo {
+    public static void main(String[] args) {
+        printCustomerName();
+        printCustomerFirstName();
+    }
+
+    public static void printCustomerFirstName() {
+        System.out.println("Имя нашего пользователя Сергей");
+    }
+
+    public static void printCustomerName() {
+        System.out.println("Фамилия нашего пользователя Иванов");
+    }
+}
+
+
+
