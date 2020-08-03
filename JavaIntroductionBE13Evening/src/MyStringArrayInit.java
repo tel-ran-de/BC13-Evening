2bb44f814991bc28d3616748518a0289b4b3cc35
andron13 <andron13@gmail.com>
2020-08-03 21:13:33 +0200

    30 arrays

---
+++ JavaIntroductionBE13Evening/src/MyStringArrayInit.java
@@ -0,0 +1,40 @@
+public class MyStringArrayInit {
+    public static void main(String[] args) {
+        int myRandom = (int) (Math.random() * 25 + 1);
+        String[] myStr = new String[myRandom];
+        myStringArrayPrint(myStr);
+
+        String[] test = fillMyStringArray(myStr);
+        myStringArrayPrint(test);
+    }
+
+    public static String[] fillMyStringArray(String[] str) {
+        for (int i = 0; i < str.length; i++) {
+            str[i] = stringGenerate(10).toLowerCase();
+        }
+        return str;
+    }
+
+    private static String stringGenerate(int length) {
+        String result = "";
+        for (int i = 0; i < length; i++) {
+            result = result + oneLetter();
+        }
+        return result;
+    }
+
+    private static char oneLetter() {
+        String source = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char letter = source.charAt((int) (Math.random() * source.length()));
+        return letter;
+    }
+
+    private static void myStringArrayPrint(String[] myStr) {
+        System.out.println("Строковый массив, длиной - " + myStr.length);
+        for (int i = 0; i < myStr.length - 1; i++) {
+            System.out.print(myStr[i] + ", ");
+        }
+        System.out.print(myStr[myStr.length - 1]);
+        System.out.println();
+    }
+}
